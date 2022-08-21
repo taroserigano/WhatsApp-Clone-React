b03075d7631cc429e5de95a0babbde62d28bac27
Taro Serigano <taroserigano@gmail.com>
2022-08-21 14:48:47 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ io.on('connection', socket => {
   socket.on('send-message', ({ recipients, text }) => {
     recipients.forEach(recipient => {
       const newRecipients = recipients.filter(r => r !== recipient) // recipient is Me! The sender, so filter out the actual sender and send only to the other people
-      newRecipients.push(id)
+      newRecipients.push(id) // adding the actual sender's ID here 
       socket.broadcast.to(recipient).emit('receive-message', { // send msg to the recipient's Room 
         recipients: newRecipients, sender: id, text            // recipients is All recipients (with new recipient) 
       })
